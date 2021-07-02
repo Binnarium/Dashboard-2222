832602eb8ba32f3085d76a782cf76b7fe3737916
Bruno Esparza <bruno.be81@gmail.com>
2021-07-01 22:02:33 -0500

    add core, shared, and auth modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // authentication module
+  { path: 'ingresar', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+
+  // redirect all inconsistent routes to home
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
