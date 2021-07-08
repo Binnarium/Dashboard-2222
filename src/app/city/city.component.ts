705b2107d6055eed59fbd7a1ee7a881fb8a53148
Bruno Esparza <bruno.be81@gmail.com>
2021-07-07 23:47:34 -0500

    add color to cities

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { LoadCityService } from '../core/cities-module/load-city.service';
 
 @Component({
   selector: 'dashboard-city',
@@ -9,11 +13,23 @@ import { Component } from '@angular/core';
       <dashboard-cities-sidebar></dashboard-cities-sidebar>
     </div>
 
-    <div class="flex-grow">
+    <div class="flex-grow" [ngStyle]="{'background-color': cityColor$|async}">
       <router-outlet></router-outlet>
     </div>
   </div>
 
   `,
 })
-export class CityComponent { }
+export class CityComponent {
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly loadCityService: LoadCityService,
+  ) { }
+
+  readonly cityColor$: Observable<string> = this.route.params.pipe(
+    map(params => params.cityId as string),
+    switchMap(cityId => this.loadCityService.city$(cityId)),
+    map(city => city?.configuration.colorHex ?? null),
+    map(color => color ? `#${(color - 0xff000000).toString(16)}` : 'inherit'),
+  );
+}
