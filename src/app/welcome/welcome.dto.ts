9c7639919dd4b2f8e3866e67c88dc2aef3b39c1e
Bruno Esparza <bruno.be81@gmail.com>
2021-07-19 00:44:44 -0500

    remove large text

--- src/app/welcome/welcome.dto.ts
+++ src/app/welcome/welcome.dto.ts
@@ -3,6 +3,5 @@ import { VideoDTO } from "../shared/upload/asset.dto";
 export interface WelcomeDto {
   pageTitle: string | null;
   profundityText: string | null;
-  largeText: string | null;
   welcomeVideo: VideoDTO;
 }
