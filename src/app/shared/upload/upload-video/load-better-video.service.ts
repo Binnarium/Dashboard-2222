893820da0808c124f23b9ba09c83cc39173409a5
Bruno Esparza <bruno.be81@gmail.com>
2021-10-24 13:55:51 -0500

    add processing video state

---
+++ src/app/shared/upload/upload-video/load-better-video.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { VideoDTO } from '../asset.dto';
+import { BetterVideoModel } from './better-video.model';
+
+@Injectable({ providedIn: 'root' })
+export class LoadBetterVideoService {
+  constructor(
+    private readonly database: AngularFireDatabase
+  ) { }
+
+
+  load$(video: VideoDTO): Observable<BetterVideoModel | null> {
+    if (!video.path)
+      return of(null);
+
+    const videoPath: string = video.path.replace(/[&/\\#, +()$~%.'":*?<>{}]/g, '_');
+
+    return this.database.object<BetterVideoModel>(`videos/${videoPath}`).snapshotChanges().pipe(
+      map(snap => snap.payload.val() ?? null)
+    );
+  }
+}
